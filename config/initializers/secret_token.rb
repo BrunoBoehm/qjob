70ea0c500e09e5e093beb60fe8b251ef693134f9
Bruno Boehm <bruno.boehm@gmail.com>
2013-09-09 20:04:38 +0200

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qjob::Application.config.secret_key_base = 'ae09ef37972fef6e7627c8219579b0dc6ec8e79e29b5a57e0d99e61f397d305d9d779acf012f67c81d976c4f7a4a13d720c28359359cd3892c71eadd77e7185b'
